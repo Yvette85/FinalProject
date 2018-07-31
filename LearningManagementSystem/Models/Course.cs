91eca7398438ad81e38f4e6c703fc0714e6262b4
Yvette85 <y.kamariza@yahoo.com>
2018-07-31 10:17:13 +0200

    Adding properties to IdentityModels

--- LearningManagementSystem/Models/Course.cs
+++ LearningManagementSystem/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,22 @@ namespace LearningManagementSystem.Models
     public class Course
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Description { get; set; }
+
         public DateTime StartDate { get; set; }
+
+     
         public virtual ICollection<ApplicationUser> Students { get; set; }
 
 
     }
 }
+
+//var isValid = DateTime.TryParseExact(Convert.ToString(value),
+//               "yyyy-mm-dd",
