28fc1199917fca42818347d86d683d8f1ef902f5
olofkarlstrand <olofkarlstrand@gmail.com>
2018-08-09 11:02:23 +0200

    new

--- LearningManagementSystem/Controllers/UserController.cs
+++ LearningManagementSystem/Controllers/UserController.cs
@@ -1,84 +1,84 @@
-﻿//using LearningManagementSystem.Models;
-//using LearningManagementSystem.Models.ViewModels;
-//using Microsoft.AspNet.Identity;
-//using Microsoft.AspNet.Identity.EntityFramework;
-//using Microsoft.AspNet.Identity.Owin;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
+﻿using LearningManagementSystem.Models;
+using LearningManagementSystem.Models.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-//namespace LearningManagementSystem.Controllers
-//{
-//    public class UserController : Controller
-//    {
-//        private ApplicationDbContext context = new ApplicationDbContext();
+namespace LearningManagementSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        private ApplicationDbContext context = new ApplicationDbContext();
 
-//        public UserManager<IdentityUser> userManager => HttpContext.GetOwinContext().Get<UserManager<IdentityUser>>();
-
-        
-//        public ActionResult Index()
-//        {
-
-//            List<ApplicationUser> studIndex = new List<ApplicationUser>();
-
-//            foreach (var s in studIndex)
-//            {
-                
-//                studIndex.Add(new ApplicationUser()
-//                {
-//                    FirstName = s.FirstName,
-//                    LastName = s.LastName,
-//                    CourseId = s.CourseId,
-//                    Email = s.Email,
-
-//                });
-
-//                return RedirectToAction("Index");
-
-//            }
-
-//            return View(context.Users.ToList());
-//        }
+        public UserManager<IdentityUser> userManager => HttpContext.GetOwinContext().Get<UserManager<IdentityUser>>();
 
 
-      
-//        // GET: User
+        public ActionResult Index()
+        {
 
-//        [Authorize(Roles = "Teacher")]
-//        public ActionResult Register()
-//        {
+            List<ApplicationUser> studIndex = new List<ApplicationUser>();
 
-//            ApplicationDbContext context = new ApplicationDbContext();
+            foreach (var s in studIndex)
+            {
 
-//            var viewModel = new RegisterViewModel();
+                studIndex.Add(new ApplicationUser()
+                {
+                    FirstName = s.FirstName,
+                    LastName = s.LastName,
+                    CourseId = s.CourseId,
+                    Email = s.Email,
 
-//            viewModel.Roles = context.Roles.ToList();
-//            viewModel.Courses = context.Courses.ToList();
+                });
 
-//            return View(viewModel);
-//        }
+                return RedirectToAction("Index");
 
-//        [HttpPost]
-//        public ActionResult Register(RegisterViewModel model)
-//        {
-//            var userStore = new UserStore<IdentityUser>();
-//            UserManager<IdentityUser> userManager = new UserManager<IdentityUser>(userStore);
+            }
 
-//            var identityResult = userManager.Create(new IdentityUser(model.Email), model.Password);
+            return View(context.Users.ToList());
+        }
 
-//            if (identityResult.Succeeded)
-//            {
-//                return RedirectToAction("Index", "Home");
-//            }
 
-//            ModelState.AddModelError("", identityResult.Errors.FirstOrDefault());
 
-//            return View(model);
-//        }
+        // GET: User
 
-//    }
-//}
+        [Authorize(Roles = "Teacher")]
+        public ActionResult Register()
+        {
+
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var viewModel = new RegisterViewModel();
+
+            viewModel.Roles = context.Roles.ToList();
+            viewModel.Courses = context.Courses.ToList();
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            UserManager<IdentityUser> userManager = new UserManager<IdentityUser>(userStore);
+
+            var identityResult = userManager.Create(new IdentityUser(model.Email), model.Password);
+
+            if (identityResult.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", identityResult.Errors.FirstOrDefault());
+
+            return View(model);
+        }
+
+    }
+}
 
 
--- LearningManagementSystem/Models/ApplicationDbContext.cs
+++ LearningManagementSystem/Models/ApplicationDbContext.cs
@@ -25,6 +25,6 @@ namespace LearningManagementSystem.Models
             return new ApplicationDbContext();
         }
 
-        public System.Data.Entity.DbSet<LearningManagementSystem.Models.ApplicationUser> ApplicationUsers { get; set; }
+       
     }
 }
