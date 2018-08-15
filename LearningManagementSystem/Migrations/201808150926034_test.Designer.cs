5ae0cfbeffd6f67e57d94549509f2b7ccd852018
zafar iqbal <rasalunic@gmail.com>
2018-08-15 11:50:16 +0200

    .......!!!

--- LearningManagementSystem/Migrations/201808150926034_test.Designer.cs
+++ LearningManagementSystem/Migrations/201808150926034_test.Designer.cs
@@ -7,13 +7,13 @@ namespace LearningManagementSystem.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class Init : IMigrationMetadata
+    public sealed partial class test : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(test));
         
         string IMigrationMetadata.Id
         {
-            get { return "201808141434171_Init"; }
+            get { return "201808150926034_test"; }
         }
         
         string IMigrationMetadata.Source
